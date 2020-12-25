cf820594eade3788d74e256b414e49b5015b2770
Dmitriy <fidim12@gmail.com>
2020-12-25 18:23:54 +0300

    first commit

---
+++ textbook/js/scripts.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// const obj = {
+//     name: 'pidar',
+//     age: 54,
+//     isMaried: false
+
+// };
+
+// console.log(obj.name);
+
+// confirm('idinahui');
+
+const lexa = prompt('ti lexa?', 'hui');
+console.log(lexa + ' na');
